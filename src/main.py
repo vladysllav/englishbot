cb77df3ce8d73f2e86e6ef6c71255d0be9c22e1c
Kostiantyn Varetsa <k.varetsa@gmail.com>
2023-06-08 14:35:57 +0200

    Applied some changes to main.py file

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, filters, CallbackContext
+from telegram.ext import CommandHandler, MessageHandler, filters, ContextTypes, Application
 from dotenv import load_dotenv
 import os
 
@@ -7,36 +7,31 @@ import os
 load_dotenv()
 
 
-class EnglishSkillsBot:
-    def __init__(self):
-        self.updater = Updater(os.getenv("TELEGRAM_TOKEN"))
-        self.dispatcher = self.updater.dispatcher
-
-    async def start(self, update: Update, context: CallbackContext):
-        await context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to the English Skills Bot! How can I assist you?")
-
-    async def handle_text(self, update: Update, context: CallbackContext):
-        # Process the user's message here and check their English skills
-        response = self.check_english_skills(update.message.text)
-        await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
-
-    def check_english_skills(self, text):
-        # Add your English skills checking logic here
-        # You can use libraries like NLTK, spaCy, or any other NLP tools
-
-        # Placeholder response for demonstration purposes
-        return "Your English skills are impressive!"
-
-    def start_bot(self):
-        start_handler = CommandHandler('start', self.start)
-        text_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, self.handle_text)
-
-        self.dispatcher.add_handler(start_handler)
-        self.dispatcher.add_handler(text_handler)
-
-        self.updater.start_polling()
-        self.updater.idle()
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Welcome to the English Skills Bot! How can I assist you?")
 
 
-bot = EnglishSkillsBot()
-bot.start_bot()
+async def handle_text(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # Process the user's message here and check their English skills
+    response = check_english_skills(update.message.text)
+    await update.message.reply_text(response)
+
+
+def check_english_skills(text):
+    # Add your English skills checking logic here
+    # You can use libraries like NLTK, spaCy, or any other NLP tools
+
+    # Placeholder response for demonstration purposes
+    return "Your English skills are impressive!"
+
+
+if __name__ == '__main__':
+    print("Starting bot...")
+    app = Application.builder().token(os.getenv("TELEGRAM_TOKEN")).build()
+
+    app.add_handler(CommandHandler('start', start))
+
+    app.add_handler(MessageHandler(filters.TEXT, handle_text))
+
+    print("Polling...")
+    app.run_polling(poll_interval=3)
