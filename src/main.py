69932c28d6d019dff95bb8af2512bfbb08efec7d
AlexGrig23 <AlexGrig.Cyber@gmail.com>
2023-06-14 14:08:37 +0300

    Fixed comments

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ load_dotenv()
 class EnglishSkillsBot:
     def __init__(self):
         self.app = Application.builder().token(os.getenv("TELEGRAM_TOKEN")).build()
-        self.callback_handler = CallbackQueryHandler(self.handle_callback)
+
 
     def create_start_button(self):
         """
@@ -47,7 +47,7 @@ class EnglishSkillsBot:
         response = self.check_english_skills(update.message.text)
         await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
 
-    async def handle_callback(self, update: Update, context: CallbackContext):
+    async def choose_level_callback(self, update: Update, context: CallbackContext):
         """
         Handle the callback query from buttons level
         :param update: The incoming update.
@@ -70,10 +70,11 @@ class EnglishSkillsBot:
     def start_bot(self):
         start_handler = CommandHandler('start', self.start)
         text_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, self.handle_text)
+        callback_handler = CallbackQueryHandler(self.choose_level_callback)
 
         self.app.add_handler(start_handler)
         self.app.add_handler(text_handler)
-        self.app.add_handler(self.callback_handler)
+        self.app.add_handler(callback_handler)
 
         self.app.run_polling()
 
