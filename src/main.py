ad46fb74fb73f2bf172f841687c74dcb30bb258e
AlexGrig23 <AlexGrig.Cyber@gmail.com>
2023-07-06 13:04:26 +0300

    Add class Quest

--- src/main.py
+++ src/main.py
@@ -53,7 +53,7 @@ class EnglishSkillsBot:
         :param context: The context object for handlers.
         """
         user_id = update.message.from_user.id
-        reply_answer = None  # TODO: remove later
+        reply_answer = None # TODO: remove later
         if self.check_user_exist(user_id):
             pass  # TODO: Create buttons with options get stats and pass ex again
         else:
@@ -61,6 +61,7 @@ class EnglishSkillsBot:
         await context.bot.send_message(chat_id=update.effective_chat.id,
                                        text="Welcome to the English Skills Bot! How can I assist you?",  # TODO: customize message depending on result
                                        reply_markup=reply_answer)
+        await self.choose_level_callback(update, context)
 
     async def statistics(self, update: Update, context: CallbackContext):
         """
@@ -79,38 +80,72 @@ class EnglishSkillsBot:
 
     async def choose_level_callback(self, update: Update, context: CallbackContext):
         """
-        Handle the callback query from buttons level
-        :param update: The incoming update.
-        :param context: The context object for handlers.
+        Gets the user id, sends the level selection buttons to the user,
+        gets the user level, then calls the send_question function, passing the level and id parameters
         """
-        user_id = 327542
+
+
+        user_id = update.message.from_user.id
+
+        reply_answer = self.create_lvl_button()
+
+        await context.bot.send_message(chat_id=update.effective_chat.id,
+                                       text="Change English level",
+                                       # TODO: customize message depending on result
+                                       reply_markup=reply_answer)
         query = update.callback_query
-        if query.data == "start":
-            reply_answer = self.create_lvl_button()
-            await query.message.reply_text("Please select your English level", reply_markup=reply_answer)
-        elif query.data == "statistics":
-            if self.user_service.check_user_exist(user_id):
-                await self.statistics(update, context)
-            else:
-                await context.bot.send_message(chat_id=update.effective_chat.id,
-                                               text="You have to answer the questions first!")
-        else:
-            level = query.data
-            await self.send_question(update, context, level)
+        user_level = query.data if query.data else None
 
-    async def send_question(self, update: Update, context: CallbackContext, level: str):
-        questions = self.get_questions(level)
+        for user in self.users_data:
+            if user.id == user_id:
+                user.level = user_level
+                break
 
-        for question in questions:
-            question_text = question['question']
-            options = question['options']
+        # elif query.data == "statistics":
+        #     if self.user_service.check_user_exist(user_id):
+        #         await self.statistics(update, context)
+        #     else:
+        #         await context.bot.send_message(chat_id=update.effective_chat.id,
+        #                                        text="You have to answer the questions first!")
+        # else:
+        #     level = query.data
+        #     await self.send_question(update, context, level)
+        await self.send_question(update, context, user_level, user_id)
+    def get_user_points(self, user_id):
+        for user in self.users_data:
+            if user.id == user_id:
+                return user.points
+        return 0
 
-            keyboard = [
-                [InlineKeyboardButton(option, callback_data=option)] for option in options
-            ]
-            reply_markup = InlineKeyboardMarkup(keyboard)
-            await context.bot.send_message(chat_id=update.effective_chat.id, text=question_text,
-                                           reply_markup=reply_markup)
+    async def send_question(self, update: Update, context: CallbackContext, user_level: str, user_id):
+        """
+        Calling the get questions method getting questions and
+        answer options sending the user buttons with a question and answer options
+        """
+        question = Question(user_id, user_level)
+        question_text, options = question.get_question() # I will modify depending on the get_question method
+
+
+        keyboard = [
+            [InlineKeyboardButton(option, callback_data=option)] for option in options
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=question_text,
+                                       reply_markup=reply_markup)
+        await self.answer_handler(update, context, user_id, question)
+
+    async def answer_handler(self, update: Update, context: CallbackContext, user_id, question):
+        """
+        We receive a response from the button, with the user's response,
+        we pass the user points parameter to the question class and call the method for scoring points
+        """
+        query = update.callback_query
+        user_points = self.get_user_points(user_id)
+        question(user_points)
+
+        user_answer = query.data
+        question.point_calculate(user_answer == question.get_answer())
+
 
     def get_questions(self, level: str):
         if level == "Beginner":
@@ -165,15 +200,36 @@ class EnglishSkillsBot:
     def start_bot(self):
         start_handler = CommandHandler('start', self.start)
         text_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, self.handle_text)
-        other_callback = CallbackQueryHandler(self.other_callback, pattern='^advanced_q1*')
+        # other_callback = CallbackQueryHandler(self.other_callback, pattern='^advanced_q1*')
         callback_handler = CallbackQueryHandler(self.choose_level_callback, pattern='^lvl_*')
+        answer_handler = CallbackQueryHandler(self.answer_handler, pattern='^[A-Z]$')
 
         self.app.add_handler(start_handler)
         self.app.add_handler(text_handler)
-        self.app.add_handler(other_callback)
+        # self.app.add_handler(other_callback)
         self.app.add_handler(callback_handler)
+        self.app.add_handler(answer_handler)
 
         self.app.run_polling()
+
+class Question:
+    def __init__(self, user_id: int, user_level: str, user_points: int = None, user_answer: str = None):
+        self.user_id = user_id
+        self.user_level = user_level
+        self.user_points = user_points
+        self.user_answer = user_answer
+
+    def get_question(self):
+        """I will finalize when the sequence of questions is ready"""
+        pass
+
+    def get_answer(self):
+        """I will finalize when the sequence of questions is ready"""
+        pass
+
+    def point_calculate(self, correct_answer: bool):
+        if correct_answer:
+            self.user_points += 1
 
 
 if __name__ == '__main__':
