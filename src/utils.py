04c49b87dd6837237d1892abf0dbb22f814082e5
truemerci <danilgladkiy7@gmail.com>
2023-07-28 16:08:42 +0300

    Implementation of the user data forgery function

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,17 @@
 from typing import List
-
+from faker import Faker
 from src.user_statistics import UserStatistics
 
 
 def fake_user_data() -> List[UserStatistics]:
-    pass
+    fake = Faker()
+    num_users = 100
+    user_data = []
+    for i in range(num_users):
+        id = fake.random_int(min=1, max=1000)
+        first_name = fake.first_name()
+        last_name = fake.last_name()
+        points = fake.random_int(min=0, max=100)
+        user = UserStatistics(id=id, first_name=first_name, last_name=last_name, points=points)
+        user_data.append(user)
+    return user_data
